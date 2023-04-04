436aaa0d75765e6eb9e0f7768d508930cbc281a8
StarpTech <deusdustin@gmail.com>
2023-04-04 12:11:58 +0200

    improve

--- app/page.tsx
+++ app/page.tsx
@@ -25,35 +25,39 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
       <div className="flex space-x-4 md:space-x-8">
-	  <img src="https://avatars.githubusercontent.com/u/1764424?v=4" className="rounded-full h-9 md:h-32 overflow-hidden duration-500 animate-title bg-clip-text"/>
+        <img
+          src="https://avatars.githubusercontent.com/u/1764424?v=4"
+          className="rounded-full h-9 md:h-32 overflow-hidden duration-500 animate-title bg-clip-text"
+        />
         <h1 className="z-10 text-4xl text-transparent duration-500 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text bg-white">
           starptech
         </h1>
       </div>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="flex flex-col my-16 text-center animate-fade-in">
-        <h2 className="text-zinc-500 max-w-3xl leading-relaxed text-sm md:text-md">
-          Hi, my name is Dustin, I'm founder and Tech Lead at{" "}
+      <div className="flex flex-col mx-4 md:mx-0 my-16 text-center animate-fade-in">
+        <h2 className="text-zinc-500 leading-relaxed text-sm md:text-md md:max-w-2xl text-center">
+        Hi, my name is Dustin, I'm founder and Tech Lead at{" "}
           <Link
             target="_blank"
             href="https://wundergraph.com"
-            className="underline duration-500 hover:text-zinc-300"
+            className="duration-500 whitespace-nowrap font-mono text-zinc-400 hover:text-zinc-300 after:content-['_↗'] after:text-sm after:font-bold px-1 after:relative after:top-[-2px] after:right-[2px]"
           >
             WunderGraph
           </Link>
         </h2>
-		<div className="text-zinc-500 leading-relaxed text-sm md:text-md max-w-2xl text-center">
-          We're building the <Link
+        <h3 className="text-zinc-500 leading-relaxed text-sm md:text-md max-w-2xl text-center">
+        We're building the{" "}
+          <Link
             target="_blank"
             href="https://samnewman.io/patterns/architectural/bff/"
-            className="underline duration-500 hover:text-zinc-300"
+            className="duration-500 whitespace-nowrap text-zinc-400 font-mono hover:text-zinc-300 after:content-['_↗'] after:text-sm after:font-bold px-1 after:relative after:top-[-2px] after:right-[2px]"
           >
             backend for frontend
-          </Link>{" "}framework. Empower your
-          developers to streamline their integration workflows to provide a
-          unified data layer for all frontend frameworks.
-		</div>
+          </Link>{" "}
+          framework. Empower your developers to streamline their integration
+          workflows to provide a unified data layer for all frontend frameworks.
+        </h3>
       </div>
     </div>
   );
